5a51b333b66f2dea8de22f3e2d4501cdcef3a1f3
Yosuke Kurami <yosuke.kurami@gmail.com>
2019-08-12 01:20:02 +0900

    first commit

---
+++ packages/reg-gh-app-front/cmrh.conf.js
@@ -0,0 +1,6 @@
+// FIXME
+// This file is copyed to the repository root dir because workspaces feature hoisted css-modules-require-hook and it can't this file.
+//
+// module.exports = {
+//   generateScopedName: '[name]_[local]',
+// };
