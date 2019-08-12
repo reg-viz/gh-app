35a378f596a7863a0a54da38b45afba9d4641ad3
Yosuke Kurami <yosuke.kurami@gmail.com>
2019-08-12 21:08:24 +0900

    expose server info using function

--- packages/reg-gh-app-interface/src/index.ts
+++ packages/reg-gh-app-interface/src/index.ts
@@ -1,1 +1,9 @@
 export * from "./event-types";
+
+export type GhAppServerInfo = {
+  endpoint: string;
+};
+
+export function getGhAppInfo(): GhAppServerInfo {
+  return require("../endpoint.json").endpoint as GhAppServerInfo;
+}
